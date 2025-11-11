f21483ee970a935aabdf0917a5e5ba3051aa1337
ydahal <yadhap.dahal@lexisnexisrisk.com>
2025-11-11 16:54:46 -0500

    Update user data - 5 records (2025-11-11T21:54:45.638Z)

---
+++ data/user-data.js
@@ -0,0 +1,27 @@
+// Generated on 11/11/2025, 16:54:45 (EST)
+// Total records: 5
+
+const users = [
+  {
+    "lName": "User",
+    "createdAt": "10/15/2025, 12:57:22"
+  },
+  {
+    "lName": "User",
+    "createdAt": "10/15/2025, 12:47:36"
+  },
+  {
+    "lName": "Bid",
+    "createdAt": "10/14/2025, 22:17:01"
+  },
+  {
+    "lName": "Dahal",
+    "createdAt": "10/09/2025, 11:27:41"
+  },
+  {
+    "lName": "Dhakal",
+    "createdAt": "09/12/2025, 05:56:23"
+  }
+];
+
+module.exports = { users };
