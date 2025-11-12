427a0a8eeddbee01af6c38dd0585449b985a59dc
ydahal <yadhap.dahal@lexisnexisrisk.com>
2025-11-11 22:44:02 -0500

    Update game data - 19 records (2025-11-12T03:44:01.543Z)

--- visualization/src/data/game-data.js
+++ visualization/src/data/game-data.js
@@ -1,118 +1,137 @@
-// Generated on 11/11/2025, 22:43:46 (EST)
+// Generated on 11/11/2025, 22:44:01 (EST)
 // Total records: 19
 
 export const games = [
   {
     "id": "0f3d264c-5f62-48c4-838d-6e727e6cf9d1",
     "name": "uu VS tt",
+    "creatorId": "110091634567525704379",
     "creatorLastName": "Dhakal",
     "createdAt": "11/09/2025, 07:03:08"
   },
   {
     "id": "9d00ebe1-037f-41cd-af5a-3d448f2c652a",
     "name": "Amilo VS Piro",
+    "creatorId": "118398045643308882603",
     "creatorLastName": "Dahal",
     "createdAt": "11/06/2025, 15:47:05"
   },
   {
     "id": "615e7336-1877-4f0a-a040-0d05c8171fef",
     "name": "Ratto VS Kalooo",
+    "creatorId": "118398045643308882603",
     "creatorLastName": "Dahal",
     "createdAt": "11/06/2025, 15:44:05"
   },
   {
     "id": "79d2eb2e-3d76-49e8-b066-dada448e770c",
     "name": "88899 VS 99999",
+    "creatorId": "118398045643308882603",
     "creatorLastName": "Dahal",
     "createdAt": "11/06/2025, 15:11:46"
   },
   {
     "id": "79af2499-e8e9-49cd-81d3-b3ede69630f7",
     "name": "ewss VS wqaw",
+    "creatorId": "118398045643308882603",
     "creatorLastName": "Dahal",
     "createdAt": "11/06/2025, 14:58:42"
   },
   {
     "id": "e3625229-3da5-48d8-8f48-b0fe45742279",
     "name": "Peesss VS peas",
+    "creatorId": "118398045643308882603",
     "creatorLastName": "Dahal",
     "createdAt": "11/06/2025, 14:46:10"
   },
   {
     "id": "1468cfd2-81d7-4e93-bee9-fc45febc0f66",
     "name": "aaxe VS yee",
+    "creatorId": "118398045643308882603",
     "creatorLastName": "Dahal",
     "createdAt": "11/06/2025, 14:45:50"
   },
   {
     "id": "ac7befc6-62fe-4b1b-a655-e9a71483623c",
     "name": "HBD VS HBDY",
+    "creatorId": "118398045643308882603",
     "creatorLastName": "Dahal",
     "createdAt": "11/06/2025, 14:45:06"
   },
   {
     "id": "d2e1799d-8a55-4ee0-b4d4-5ddc1235dabb",
     "name": "Amanana VS amamammamama",
+    "creatorId": "118398045643308882603",
     "creatorLastName": "Dahal",
     "createdAt": "11/06/2025, 14:39:43"
   },
   {
     "id": "800b57d7-e611-4976-8a60-2fd409aa4056",
     "name": "A VS C",
+    "creatorId": "118398045643308882603",
     "creatorLastName": "Dahal",
     "createdAt": "11/06/2025, 11:34:27"
   },
   {
     "id": "2e7127c5-d85c-4434-b753-fb9cb856f1ea",
     "name": "Bhutan VS Nepal",
+    "creatorId": "118398045643308882603",
     "creatorLastName": "Dahal",
     "createdAt": "11/06/2025, 11:33:49"
   },
   {
     "id": "70ef72f4-1ae2-44e4-a204-4070d191b38e",
     "name": "Happy VS Birthday",
+    "creatorId": "118398045643308882603",
     "creatorLastName": "Dahal",
     "createdAt": "11/06/2025, 11:25:19"
   },
   {
     "id": "7964ebdf-e0e8-4ea7-a2aa-6e90aa5454a5",
     "name": "Player 1 VS Player 2",
+    "creatorId": "118398045643308882603",
     "creatorLastName": "Dahal",
     "createdAt": "11/06/2025, 08:25:06"
   },
   {
     "id": "8e6eb695-d52d-4ca3-8de8-e74f5bfe830b",
     "name": "Bam VS Name",
+    "creatorId": "118398045643308882603",
     "creatorLastName": "Dahal",
     "createdAt": "11/05/2025, 22:44:00"
   },
   {
     "id": "f30b8157-24eb-4139-b8ca-e75e8e27777e",
     "name": "ape VS dape",
+    "creatorId": "118398045643308882603",
     "creatorLastName": "Dahal",
     "createdAt": "11/05/2025, 22:26:49"
   },
   {
     "id": "218e874d-1df5-451c-b742-328959263506",
     "name": "Alu VS Kha",
+    "creatorId": "118398045643308882603",
     "creatorLastName": "Dahal",
     "createdAt": "10/27/2025, 23:04:24"
   },
   {
     "id": "45f22c55-7628-465c-bbf0-a02d58799540",
     "name": "Kalu Bhadur VS Alu Bahadur",
+    "creatorId": "118398045643308882603",
     "creatorLastName": "Dahal",
     "createdAt": "10/27/2025, 21:42:45"
   },
   {
     "id": "ccc2158e-acc8-4a19-9544-e2e3cff2ce3d",
     "name": "Yadhap VS Dahal",
+    "creatorId": "118398045643308882603",
     "creatorLastName": "Dahal",
     "createdAt": "10/25/2025, 07:45:52"
   },
   {
     "id": "77b22409-e437-4ea7-a2fa-27355a832944",
     "name": "Yadhap Dahal2",
+    "creatorId": "110091634567525704379",
     "creatorLastName": "Dhakal",
     "createdAt": "10/23/2025, 11:55:25"
   }
