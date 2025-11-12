fd86fbd155a574abc36fb0fc64ddc11d43b95158
ydahal <yadhap.dahal@lexisnexisrisk.com>
2025-11-11 22:36:32 -0500

    Update game data - 19 records (2025-11-12T03:36:31.699Z)

---
+++ visualization/src/data/game-data.js
@@ -0,0 +1,100 @@
+// Generated on 11/11/2025, 22:36:31 (EST)
+// Total records: 19
+
+export const games = [
+  {
+    "name": "uu VS tt",
+    "creatorLastName": "Dhakal",
+    "createdAt": "11/09/2025, 07:03:08"
+  },
+  {
+    "name": "Amilo VS Piro",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/06/2025, 15:47:05"
+  },
+  {
+    "name": "Ratto VS Kalooo",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/06/2025, 15:44:05"
+  },
+  {
+    "name": "88899 VS 99999",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/06/2025, 15:11:46"
+  },
+  {
+    "name": "ewss VS wqaw",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/06/2025, 14:58:42"
+  },
+  {
+    "name": "Peesss VS peas",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/06/2025, 14:46:10"
+  },
+  {
+    "name": "aaxe VS yee",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/06/2025, 14:45:50"
+  },
+  {
+    "name": "HBD VS HBDY",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/06/2025, 14:45:06"
+  },
+  {
+    "name": "Amanana VS amamammamama",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/06/2025, 14:39:43"
+  },
+  {
+    "name": "A VS C",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/06/2025, 11:34:27"
+  },
+  {
+    "name": "Bhutan VS Nepal",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/06/2025, 11:33:49"
+  },
+  {
+    "name": "Happy VS Birthday",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/06/2025, 11:25:19"
+  },
+  {
+    "name": "Player 1 VS Player 2",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/06/2025, 08:25:06"
+  },
+  {
+    "name": "Bam VS Name",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/05/2025, 22:44:00"
+  },
+  {
+    "name": "ape VS dape",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/05/2025, 22:26:49"
+  },
+  {
+    "name": "Alu VS Kha",
+    "creatorLastName": "Dahal",
+    "createdAt": "10/27/2025, 23:04:24"
+  },
+  {
+    "name": "Kalu Bhadur VS Alu Bahadur",
+    "creatorLastName": "Dahal",
+    "createdAt": "10/27/2025, 21:42:45"
+  },
+  {
+    "name": "Yadhap VS Dahal",
+    "creatorLastName": "Dahal",
+    "createdAt": "10/25/2025, 07:45:52"
+  },
+  {
+    "name": "Yadhap Dahal2",
+    "creatorLastName": "Dhakal",
+    "createdAt": "10/23/2025, 11:55:25"
+  }
+];
