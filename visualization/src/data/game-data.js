b4e4ac72353ad41605c700eac7ee77a93e54ad89
ydahal <yadhap.dahal@lexisnexisrisk.com>
2025-11-29 23:45:02 -0500

    Update game data - 784 records (2025-11-30T04:45:01.649Z)

--- visualization/src/data/game-data.js
+++ visualization/src/data/game-data.js
@@ -1,7 +1,14 @@
-// Generated on 11/29/2025, 23:30:01 (EST)
-// Total records: 783
+// Generated on 11/29/2025, 23:45:01 (EST)
+// Total records: 784
 
 export const games = [
+  {
+    "id": "c223662a-2478-4d12-a2b6-f4788be6df9f",
+    "name": "CHAMPIONSHIP",
+    "creatorId": "114705993740738417365",
+    "creatorLastName": "Jaewon",
+    "createdAt": "11/29/2025, 23:33:56"
+  },
   {
     "id": "5b018b28-43f5-4516-9f17-dacca0e5d46b",
     "name": "CSC Edition",
