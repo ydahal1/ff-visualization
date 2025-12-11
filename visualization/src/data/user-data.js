caae8ecd245e9e2e3571d3230ab9597a417cd77a
ydahal <yadhap.dahal@lexisnexisrisk.com>
2025-12-11 17:30:01 -0500

    Update user data - 990 records (2025-12-11T22:30:00.659Z)

--- visualization/src/data/user-data.js
+++ visualization/src/data/user-data.js
@@ -1,7 +1,12 @@
-// Generated on 12/11/2025, 17:15:00 (EST)
-// Total records: 989
+// Generated on 12/11/2025, 17:30:00 (EST)
+// Total records: 990
 
 export const users = [
+  {
+    "id": "100838950700237003780",
+    "lName": "Markus",
+    "createdAt": "12/11/2025, 17:25:55"
+  },
   {
     "id": "112923636899771943065",
     "lName": "Pepper",
