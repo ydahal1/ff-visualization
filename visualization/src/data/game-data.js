8ea83f4df43807bacce1e0fcd3bea89cd404bed4
ydahal <yadhap.dahal@lexisnexisrisk.com>
2025-12-17 01:45:02 -0500

    Update game data - 1655 records (2025-12-17T06:45:01.907Z)

--- visualization/src/data/game-data.js
+++ visualization/src/data/game-data.js
@@ -1,7 +1,21 @@
-// Generated on 12/17/2025, 01:30:01 (EST)
-// Total records: 1653
+// Generated on 12/17/2025, 01:45:01 (EST)
+// Total records: 1655
 
 export const games = [
+  {
+    "id": "3d55d54f-f864-42c1-b633-d06a440fd102",
+    "name": "game 7",
+    "creatorId": "105972556289124640687",
+    "creatorLastName": "Miller",
+    "createdAt": "12/17/2025, 01:40:49"
+  },
+  {
+    "id": "88a47168-8143-49ee-be7a-ad24db89be9c",
+    "name": "HEINEKEN NY",
+    "creatorId": "109587949753943729390",
+    "creatorLastName": "Mitchell",
+    "createdAt": "12/17/2025, 01:39:47"
+  },
   {
     "id": "1bb29d2c-31cb-498e-88a7-f5cb9d2e84ee",
     "name": "Game 6",
