bf45efa5aefeb577cfbb2b90ccc9ca8c0e556c7c
ydahal <yadhap.dahal@lexisnexisrisk.com>
2025-12-19 13:00:01 -0500

    Update user data - 1214 records (2025-12-19T18:00:00.373Z)

--- visualization/src/data/user-data.js
+++ visualization/src/data/user-data.js
@@ -1,7 +1,12 @@
-// Generated on 12/19/2025, 12:45:00 (EST)
-// Total records: 1213
+// Generated on 12/19/2025, 13:00:00 (EST)
+// Total records: 1214
 
 export const users = [
+  {
+    "id": "115479851697656929739",
+    "lName": "Paxinos",
+    "createdAt": "12/19/2025, 12:58:56"
+  },
   {
     "id": "101823164519037760808",
     "lName": "Neah",
