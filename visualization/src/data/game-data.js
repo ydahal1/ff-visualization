42620c91107c75931c578d71b696dd6961503697
ydahal <yadhap.dahal@lexisnexisrisk.com>
2025-12-31 18:00:02 -0500

    Update game data - 2699 records (2025-12-31T23:00:02.329Z)

--- visualization/src/data/game-data.js
+++ visualization/src/data/game-data.js
@@ -1,7 +1,28 @@
-// Generated on 12/31/2025, 17:45:02 (EST)
-// Total records: 2696
+// Generated on 12/31/2025, 18:00:02 (EST)
+// Total records: 2699
 
 export const games = [
+  {
+    "id": "d69bd20e-4761-4ed3-9f44-b0257d74e999",
+    "name": "h",
+    "creatorId": "100168197996867547526",
+    "creatorLastName": "Jaramillo",
+    "createdAt": "12/31/2025, 17:59:56"
+  },
+  {
+    "id": "b700eaa3-b381-4795-b1da-cf38b705ef64",
+    "name": "New year",
+    "creatorId": "118181488845157359539",
+    "creatorLastName": "Devunuri",
+    "createdAt": "12/31/2025, 17:59:04"
+  },
+  {
+    "id": "28374957-bf9e-44c1-bf6c-142e84edb92e",
+    "name": "Test",
+    "creatorId": "109443271404986927568",
+    "creatorLastName": "Westlund",
+    "createdAt": "12/31/2025, 17:50:20"
+  },
   {
     "id": "5c88fd1c-6996-4b42-9989-ac63c776593a",
     "name": "game 3",
