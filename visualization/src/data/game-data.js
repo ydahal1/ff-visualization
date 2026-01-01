1539f8619875b9f703a969e1465352e6aa5bf03b
ydahal <yadhap.dahal@lexisnexisrisk.com>
2026-01-01 18:30:02 -0500

    Update game data - 2725 records (2026-01-01T23:30:02.303Z)

--- visualization/src/data/game-data.js
+++ visualization/src/data/game-data.js
@@ -1,7 +1,14 @@
-// Generated on 01/01/2026, 18:15:03 (EST)
-// Total records: 2724
+// Generated on 01/01/2026, 18:30:02 (EST)
+// Total records: 2725
 
 export const games = [
+  {
+    "id": "519f2aaf-a60d-4827-83ec-2f2717cac778",
+    "name": "test",
+    "creatorId": "112632528129917553861",
+    "creatorLastName": "versteegen",
+    "createdAt": "01/01/2026, 18:28:23"
+  },
   {
     "id": "c639fe8e-e1c5-4f00-960e-5803a6cc601f",
     "name": "3",
