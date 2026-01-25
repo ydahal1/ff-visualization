3a4aac27f4b601d7fb44c7402bc1c6d15e42e59b
ydahal <yadhapdahal111@gmail.com>
2026-01-25 06:35:01 -0500

    Update game data - 32 records (2026-01-25T11:35:00.910Z)

--- visualization/src/data/game-data.js
+++ visualization/src/data/game-data.js
@@ -1,4 +1,229 @@
-// Generated on 01/25/2026, 06:34:01 (EST)
-// Total records: 0
+// Generated on 01/25/2026, 06:35:00 (EST)
+// Total records: 32
 
-export const games = [];
+export const games = [
+  {
+    "id": "a3773c9a-3e31-4cc2-b5e0-969b3f47cdd8",
+    "name": "asdadadadadadfafsdes",
+    "creatorId": "118398045643308882603",
+    "creatorLastName": "Dahal",
+    "createdAt": "01/17/2026, 09:50:55"
+  },
+  {
+    "id": "ae9fb5e3-c95a-4494-993d-85b65f46f12e",
+    "name": "asfafa",
+    "creatorId": "118398045643308882603",
+    "creatorLastName": "Dahal",
+    "createdAt": "01/17/2026, 07:26:48"
+  },
+  {
+    "id": "9a2af416-c261-4509-bf62-6dd6f2582fd7",
+    "name": "hhhh",
+    "creatorId": "118398045643308882603",
+    "creatorLastName": "Dahal",
+    "createdAt": "01/17/2026, 06:32:37"
+  },
+  {
+    "id": "fe68c804-4563-43fd-ac19-da76b4168a50",
+    "name": "Happyggg",
+    "creatorId": "110091634567525704379",
+    "creatorLastName": "Feud Online",
+    "createdAt": "01/16/2026, 09:34:17"
+  },
+  {
+    "id": "96be0738-1bac-44e9-9d52-97fbeea4a268",
+    "name": "Happy",
+    "creatorId": "103619154637744658783",
+    "creatorLastName": "Bid",
+    "createdAt": "01/16/2026, 09:00:19"
+  },
+  {
+    "id": "25235d5b-f4e5-48ff-a3b1-455b66a432f4",
+    "name": "domas",
+    "creatorId": "103619154637744658783",
+    "creatorLastName": "Bid",
+    "createdAt": "01/16/2026, 08:35:33"
+  },
+  {
+    "id": "6c3ff757-001d-4ae1-86b0-cf179328dbcd",
+    "name": "Henry",
+    "creatorId": "103619154637744658783",
+    "creatorLastName": "Bid",
+    "createdAt": "01/16/2026, 08:35:17"
+  },
+  {
+    "id": "fc0865e7-dde1-4be0-b4a5-404fdd6d49a5",
+    "name": "Alu",
+    "creatorId": "110091634567525704379",
+    "creatorLastName": "Feud Online",
+    "createdAt": "01/15/2026, 21:37:35"
+  },
+  {
+    "id": "51750bc8-6c41-4eff-b33e-2611957bcae8",
+    "name": "sssApes",
+    "creatorId": "110091634567525704379",
+    "creatorLastName": "Feud Online",
+    "createdAt": "01/15/2026, 21:32:53"
+  },
+  {
+    "id": "9be4ac89-1387-490e-abda-88821e1c88d8",
+    "name": "Happy",
+    "creatorId": "110091634567525704379",
+    "creatorLastName": "Feud Online",
+    "createdAt": "01/15/2026, 21:32:05"
+  },
+  {
+    "id": "8d2d0923-e371-4c37-ad01-a732c3eebff2",
+    "name": "T and V",
+    "creatorId": "110091634567525704379",
+    "creatorLastName": "Feud Online",
+    "createdAt": "12/04/2025, 08:51:39"
+  },
+  {
+    "id": "dc63024a-1ae4-475c-a8bc-d0ad6a968fb8",
+    "name": "Dahala Family",
+    "creatorId": "110091634567525704379",
+    "creatorLastName": "Feud Online",
+    "createdAt": "11/29/2025, 07:37:10"
+  },
+  {
+    "id": "667fca5a-1b9a-4683-8260-e27ace590076",
+    "name": "First Game",
+    "creatorId": "110091634567525704379",
+    "creatorLastName": "Feud Online",
+    "createdAt": "11/29/2025, 06:42:22"
+  },
+  {
+    "id": "5d0654f0-20c4-43e0-8993-213fd79ad21f",
+    "name": "Ratto VS pitto",
+    "creatorId": "118398045643308882603",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/16/2025, 07:04:20"
+  },
+  {
+    "id": "0f3d264c-5f62-48c4-838d-6e727e6cf9d1",
+    "name": "uu VS tt",
+    "creatorId": "118398045643308882603",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/09/2025, 07:03:08"
+  },
+  {
+    "id": "9d00ebe1-037f-41cd-af5a-3d448f2c652a",
+    "name": "Amilo VS Piro",
+    "creatorId": "118398045643308882603",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/06/2025, 15:47:05"
+  },
+  {
+    "id": "615e7336-1877-4f0a-a040-0d05c8171fef",
+    "name": "Ratto VS Kalooo",
+    "creatorId": "118398045643308882603",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/06/2025, 15:44:05"
+  },
+  {
+    "id": "79d2eb2e-3d76-49e8-b066-dada448e770c",
+    "name": "88899 VS 99999",
+    "creatorId": "118398045643308882603",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/06/2025, 15:11:46"
+  },
+  {
+    "id": "79af2499-e8e9-49cd-81d3-b3ede69630f7",
+    "name": "ewss VS wqaw",
+    "creatorId": "118398045643308882603",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/06/2025, 14:58:42"
+  },
+  {
+    "id": "e3625229-3da5-48d8-8f48-b0fe45742279",
+    "name": "Peesss VS peas",
+    "creatorId": "118398045643308882603",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/06/2025, 14:46:10"
+  },
+  {
+    "id": "1468cfd2-81d7-4e93-bee9-fc45febc0f66",
+    "name": "aaxe VS yee",
+    "creatorId": "118398045643308882603",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/06/2025, 14:45:50"
+  },
+  {
+    "id": "ac7befc6-62fe-4b1b-a655-e9a71483623c",
+    "name": "HBD VS HBDY",
+    "creatorId": "118398045643308882603",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/06/2025, 14:45:06"
+  },
+  {
+    "id": "d2e1799d-8a55-4ee0-b4d4-5ddc1235dabb",
+    "name": "Amanana VS amamammamama",
+    "creatorId": "118398045643308882603",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/06/2025, 14:39:43"
+  },
+  {
+    "id": "800b57d7-e611-4976-8a60-2fd409aa4056",
+    "name": "A VS C",
+    "creatorId": "118398045643308882603",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/06/2025, 11:34:27"
+  },
+  {
+    "id": "2e7127c5-d85c-4434-b753-fb9cb856f1ea",
+    "name": "Bhutan VS Nepal",
+    "creatorId": "118398045643308882603",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/06/2025, 11:33:49"
+  },
+  {
+    "id": "70ef72f4-1ae2-44e4-a204-4070d191b38e",
+    "name": "Happy VS Birthday",
+    "creatorId": "118398045643308882603",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/06/2025, 11:25:19"
+  },
+  {
+    "id": "7964ebdf-e0e8-4ea7-a2aa-6e90aa5454a5",
+    "name": "Player 1 VS Player 2",
+    "creatorId": "118398045643308882603",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/06/2025, 08:25:06"
+  },
+  {
+    "id": "8e6eb695-d52d-4ca3-8de8-e74f5bfe830b",
+    "name": "Bam VS Name",
+    "creatorId": "118398045643308882603",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/05/2025, 22:44:00"
+  },
+  {
+    "id": "f30b8157-24eb-4139-b8ca-e75e8e27777e",
+    "name": "ape VS dape",
+    "creatorId": "118398045643308882603",
+    "creatorLastName": "Dahal",
+    "createdAt": "11/05/2025, 22:26:49"
+  },
+  {
+    "id": "218e874d-1df5-451c-b742-328959263506",
+    "name": "Alu VS Kha",
+    "creatorId": "118398045643308882603",
+    "creatorLastName": "Dahal",
+    "createdAt": "10/27/2025, 23:04:24"
+  },
+  {
+    "id": "45f22c55-7628-465c-bbf0-a02d58799540",
+    "name": "Kalu Bhadur VS Alu Bahadur",
+    "creatorId": "118398045643308882603",
+    "creatorLastName": "Dahal",
+    "createdAt": "10/27/2025, 21:42:45"
+  },
+  {
+    "id": "ccc2158e-acc8-4a19-9544-e2e3cff2ce3d",
+    "name": "Yadhap VS Dahal",
+    "creatorId": "118398045643308882603",
+    "creatorLastName": "Dahal",
+    "createdAt": "10/25/2025, 07:45:52"
+  }
+];
