ff85ec33047944f93f3f728b84b9b78764f560d8
ydahal <yadhapdahal111@gmail.com>
2026-01-25 06:32:01 -0500

    Update user data - 0 records (2026-01-25T11:32:00.682Z)

--- visualization/src/data/user-data.js
+++ visualization/src/data/user-data.js
@@ -1,30 +1,4 @@
-// Generated on 01/25/2026, 06:31:00 (EST)
-// Total records: 5
+// Generated on 01/25/2026, 06:32:00 (EST)
+// Total records: 0
 
-export const users = [
-  {
-    "id": "110091634567525704379",
-    "lName": "Feud Online",
-    "createdAt": "01/16/2026, 17:02:07"
-  },
-  {
-    "id": "00000000000000000002",
-    "lName": "User",
-    "createdAt": "10/15/2025, 12:57:22"
-  },
-  {
-    "id": "00000000000000000000",
-    "lName": "User",
-    "createdAt": "10/15/2025, 12:47:36"
-  },
-  {
-    "id": "103619154637744658783",
-    "lName": "Bid",
-    "createdAt": "10/14/2025, 22:17:01"
-  },
-  {
-    "id": "118398045643308882603",
-    "lName": "Dahal",
-    "createdAt": "10/09/2025, 11:27:41"
-  }
-];
+export const users = [];
