f87bd9fd87a8f2f10d26cff2ec5f4ae2b8d1d5d3
ydahal <yadhapdahal111@gmail.com>
2026-02-02 14:24:01 -0500

    Update user data - 1 records (2026-02-02T19:24:00.735Z)

--- visualization/src/data/user-data.js
+++ visualization/src/data/user-data.js
@@ -1,4 +1,10 @@
-// Generated on 02/02/2026, 14:23:00 (EST)
-// Total records: 0
+// Generated on 02/02/2026, 14:24:00 (EST)
+// Total records: 1
 
-export const users = [];
+export const users = [
+  {
+    "id": "00000000000000000000",
+    "lName": "User",
+    "createdAt": "02/02/2026, 14:23:02"
+  }
+];
