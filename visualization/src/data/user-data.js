24a116bc1a9f4eda5c462ab0371e1970ae7ba029
ydahal <yadhapdahal111@gmail.com>
2026-02-02 13:30:00 -0500

    Update user data - 2 records (2026-02-02T18:30:00.120Z)

--- visualization/src/data/user-data.js
+++ visualization/src/data/user-data.js
@@ -1,7 +1,12 @@
-// Generated on 02/02/2026, 13:29:01 (EST)
-// Total records: 1
+// Generated on 02/02/2026, 13:30:00 (EST)
+// Total records: 2
 
 export const users = [
+  {
+    "id": "110091634567525704379",
+    "lName": "Feud Online",
+    "createdAt": "02/02/2026, 13:29:30"
+  },
   {
     "id": "00000000000000000000",
     "lName": "User",
